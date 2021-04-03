952f91d11d523aa205d44db3d43c692f4fe1e0b5
Shane Maglangit <shanedmaglangit@gmail.com>
2021-04-03 14:42:21 +0800

    Add html serving

--- adventure/adventure.go
+++ adventure/adventure.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -17,16 +20,49 @@ type Arc struct {
 	}
 }
 
+var arcs map[string]Arc
+
 func Adventure() {
 	// Get the flags
 	jsonFilename := flag.String("storyJson", "adventure/story.json", "contains the JSON data used to create the stories")
 	flag.Parse()
 
+	// Extract the stories
+	err := loadStories(jsonFilename)
+	if err != nil {
+		exit("", err)
+	}
+
+	// Start the server
+	http.HandleFunc("/", defaultHandler)
+	fmt.Println("Starting the server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	// Get the slug from the url
+	slug := r.URL.Path[len("/"):]
+
+	// Redirect to intro as the default page
+	if slug == "" {
+		http.Redirect(w, r, "intro", http.StatusFound)
+		return
+	}
+
+	// Parse the HTML template
+	pageTemplate, _ := template.ParseFiles("adventure/story.html")
+
+	// Load the page
+	if content, ok := arcs[slug]; ok {
+		_ = pageTemplate.Execute(w, content)
+	}
+}
+
+func loadStories(jsonFilename *string) error {
 	// Get the file
 	file, err := os.Open(*jsonFilename)
 	if err != nil {
-		exit(fmt.Sprintln("An error encountered while trying to find the file ::", *jsonFilename))
-		return
+		return err
 	}
 
 	// Close the file later
@@ -35,26 +71,19 @@ func Adventure() {
 	// Read content
 	jsonByte, err := ioutil.ReadAll(file)
 	if err != nil {
-		exit(fmt.Sprintln("An error encountered while trying to read the file", err))
-		return
+		return err
 	}
 
 	// Parse the Json content
-	var arcs map[string]Arc
 	err = json.Unmarshal(jsonByte, &arcs)
 	if err != nil {
-		exit(fmt.Sprintln("An error encountered while trying to parse the file ::", err))
-		return
+		return err
 	}
 
-	fmt.Println("Title", arcs["intro"].Title)
-
-	for _, paragraph := range arcs["intro"].Story {
-		fmt.Println(paragraph)
-	}
+	return nil
 }
 
-func exit(msg string) {
-	fmt.Println(msg)
+func exit(msg string, err error) {
+	fmt.Println(msg, err)
 	os.Exit(1)
 }
